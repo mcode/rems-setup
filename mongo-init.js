7f80f7b7a9727780dd94d1175ea01c40a7aa89a6
Sahil Malhotra <smalhotra@mitre.org>
2023-12-08 14:45:19 -0500

    fix failing mongo + docker-compose health check

--- mongo-init.js
+++ mongo-init.js
@@ -1,25 +1,20 @@
 // Create Databases
 const dbPims = db.getSiblingDB('pims');
 const dbRemsAdmin = db.getSiblingDB('remsadmin');
-dbRemsAdmin.createUser({ user: "rems-user",
-  pwd: "pass",
-  roles: [
-    { role: "readWrite", db: "remsadmin" } 
-  ]
-})
-// Create Collections
-dbPims.createCollection('pims-tmp');
-dbRemsAdmin.createCollection('remsadmin-tmp');
+
+if (dbRemsAdmin.getUser("rems-user") == null) {  
+    dbRemsAdmin.createUser({ user: "rems-user",
+    pwd: "pass",
+    roles: [{ role: "readWrite", db: "remsadmin" }]
+  })
+}
 
 // add the administrator user
 const dbAdmin = db.getSiblingDB('admin');
-dbAdmin.createUser({ user: "rems-admin-pims-root",
-    pwd: "rems-admin-pims-password",
-    roles: [
-        { role: "userAdminAnyDatabase", db: "admin" }
-    ]
-})
 
-// // Insert document to ensure db/collection is created
-// dbPims.pimscollection.insertOne({name: 'Hello World!'})
-// dbRemsAdmin.remsadmincollection.insertOne({name: 'Hello World Again!'})
+if (dbAdmin.getUser("rems-admin-pims-root") == null) {  
+    dbAdmin.createUser({ user: "rems-admin-pims-root",
+    pwd: "rems-admin-pims-password",
+    roles: [{ role: "userAdminAnyDatabase", db: "admin" }]
+  })
+}
