a4e9b1a1e6afa722dcf6a6da6e16717ccafdf685
Joyce Quach <jquach@mitre.org>
2024-07-30 11:04:54 -0400

    Revert "Remove unused code"
    This reverts commit 079e9a77e937e40c6c37d33ae3db3dd4f745439c.

--- mongo-init.js
+++ mongo-init.js
@@ -20,3 +20,6 @@ dbAdmin.createUser({ user: "rems-admin-pims-root",
     ]
 })
 
+// // Insert document to ensure db/collection is created
+// dbPims.pimscollection.insertOne({name: 'Hello World!'})
+// dbRemsAdmin.remsadmincollection.insertOne({name: 'Hello World Again!'})
